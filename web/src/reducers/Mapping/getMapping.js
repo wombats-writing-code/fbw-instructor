446ad3209e403ae659c0b44ec9ec10cfc361c4de
cjshaw <cjshaw@mit.edu>
2016-12-08 14:36:29 -0500

    fix mapping to use new department-based endpoints

--- web/src/reducers/Mapping/getMapping.js
+++ web/src/reducers/Mapping/getMapping.js
@@ -17,19 +17,20 @@ export function receiveMapping(mapping) {
   return {type: RECEIVE_MAPPING, mapping};
 }
 
-// returns a list of Mission offereds
+// returns all the data from Handcar
 export function getMapping(departmentName) {
 
   return function(dispatch) {
-    let modulesUrl = `${getDomain()}/middleman/objectivebanks/${departmentName}/modules`;
-    let outcomesUrl = `${getDomain()}/middleman/objectivebanks/${departmentName}/outcomes`;
-    let relationshipsUrl = `${getDomain()}/middleman/objectivebanks/${departmentName}/relationships`;
+    console.log('department for mapping', departmentName)
+    let modulesUrl = `${getDomain()}/middleman/departments/${departmentName}/modules`;
+    let outcomesUrl = `${getDomain()}/middleman/departments/${departmentName}/outcomes`;
+    let relationshipsUrl = `${getDomain()}/middleman/departments/${departmentName}/relationships`;
 
     return axios.all([axios.get(modulesUrl), axios.get(outcomesUrl), axios.get(relationshipsUrl)])
     .then(axios.spread((modules, outcomes, relationships) => {
-      // console.log('received getting mapping', res);
+      // console.log('received getting mapping', modules, outcomes, relationships);
 
-      dispatch(receiveMapping({modules: modules, outcomes: outcomes, relationships: relationships}));
+      dispatch(receiveMapping({modules: modules.data, outcomes: outcomes.data, relationships: relationships.data}));
     }))
     .catch((error) => {
       console.log('error getting mapping', error);
