bd8fb4edf49d4a68ee788e30257f743ce1497678
cjshaw <cjshaw@mit.edu>
2016-12-08 12:54:30 -0500

    clean up login methods for visitor

--- web/src/routes/Login/LoginContainer.js
+++ web/src/routes/Login/LoginContainer.js
@@ -1,11 +1,9 @@
 import { connect } from 'react-redux'
 import Login from './Login'
 
-import { logInUser } from '../../reducers/User/logInUser'
 import { updateUsername } from '../../reducers/User/updateUsername'
 import { setVisitorLogin } from '../../reducers/User/setVisitorLogin'
-import { setD2LAuthenticatedUrl } from '../../reducers/User/setD2LAuthenticatedUrl'
-import { setEnrolledSubjects } from '../../reducers/Subject/setEnrolledSubjects'
+import { setBanks } from '../../reducers/Bank/setBanks'
 
 import { logOutUser } from '../../reducers/User/logOutUser'
 
@@ -20,11 +18,11 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onSetD2LAuthenticatedUrl: (data) => dispatch(setD2LAuthenticatedUrl(data)),
-    onSetEnrolledSubjects: (bankIds) => dispatch(setEnrolledSubjects(bankIds)),
     updateUsername: (username) => dispatch(updateUsername(username)),
-    onSetVisitorLogin: (data) => dispatch(setVisitorLogin(data)),
-    login: (school, username) => dispatch(logInUser(school, username)),
+    login: (school, username) => {
+      dispatch(setBanks(null))
+      dispatch(setVisitorLogin(school, username))
+    },
     logout: () => {
       dispatch(logOutUser())
     }
