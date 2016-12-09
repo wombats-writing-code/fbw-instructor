3ee0a7c7e9690599815052a3612d26c7ce3d9e74
luwen-huang <huang.luwen@gmail.com>
2016-12-09 07:27:10 -0700

    omit results from localstorage

--- web/src/store/createStore.js
+++ web/src/store/createStore.js
@@ -2,6 +2,7 @@ import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { browserHistory } from 'react-router'
 import persistState from 'redux-localstorage'
+import _ from 'lodash'
 
 import makeRootReducer from '../reducers/reducers'
 import { updateLocation } from '../reducers/location'
@@ -22,8 +23,22 @@ export default (initialState = {}) => {
       enhancers.push(devToolsExtension())
     }
   }
+
   // copy state to local storage
-  enhancers.push(persistState())
+  enhancers.push(persistState(null, {
+    slicer: paths => state => {
+      let subset = {
+        bank: state.bank,
+        location: state.location,
+        mapping: state.mapping,
+        mission: _.omit(state.mission, ['phaseIResults', 'phaseIIResults']),
+        user: state.user,
+        view: state.view
+      };
+
+      return subset;
+    }
+  }))
 
   // ======================================================
   // Store Instantiation and HMR Setup
