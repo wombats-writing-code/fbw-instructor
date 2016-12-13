842180735ce41b5b8c69189331174957b35a31a8
cjshaw <cjshaw@mit.edu>
2016-12-13 15:11:06 -0500

    add styling for logout

--- web/src/components/NavBar/NavBar.js
+++ web/src/components/NavBar/NavBar.js
@@ -5,19 +5,15 @@ import './NavBar.scss'
 export default (props) => {
 
   return (
-    <div className="nav-bar">
+    <div className="nav-bar flex-container align-center space-between">
       <h1 className="app-title">
         <span className="fbw-name">Fly-by-Wire</span>
         <span className="app-name">Instructor App</span>
       </h1>
-      <button className="logout-button" tabIndex="1" onClick={this._logout}>
+      <button className="logout-button" tabIndex="1" onClick={props.logout}>
         Logout
       </button>
     </div>
   )
 
-  _logout = () => {
-    browserHistory.push('/login')
-    this.props.logout();
-  }
 }
