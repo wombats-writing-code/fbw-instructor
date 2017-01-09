870f763efa427fd463aa89f463d82b0e9aab621b
cjshaw <cjshaw@mit.edu>
2017-01-09 13:38:24 -0500

    fix callback on dev credentials so our dev server works with d2l

--- web/src/d2lcredentials/_dev.credentials.js
+++ web/src/d2lcredentials/_dev.credentials.js
@@ -8,7 +8,7 @@ let credentials = {
   appKey: 'n-UhtW-ADW-hrhlVmJpHXQ',
   host: process.env.NODE_ENV === 'production' ? 'https://acc.desire2learn.com' : 'http://localhost:8888/mock-d2l',
   port: process.env.NODE_ENV === 'production' ? 443 : 80,     // so that the valence lib doesn't append a :port to the string.
-  callbackUrl: process.env.NODE_ENV === 'production' ? 'https://fbw-instructor.mit.edu/d2l-callback' : 'http://localhost:3000'
+  callbackUrl: process.env.NODE_ENV === 'production' ? 'https://fbw-instructor-dev.mit.edu/d2l-callback' : 'http://localhost:3000'
 }
 
 
