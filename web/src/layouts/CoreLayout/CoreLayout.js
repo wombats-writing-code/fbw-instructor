aae823745488454d297950086a60313b9a61f6f8
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-09-19 15:31:41 -0400

    add live announcer so platform-common does not break

--- web/src/layouts/CoreLayout/CoreLayout.js
+++ web/src/layouts/CoreLayout/CoreLayout.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import NavBar from 'fbw-platform-common/components/nav-bar/web'
+import { LiveAnnouncer } from 'react-aria-live'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
-    <NavBar {...children.props} appName={"Instructor"} />
-    <div className='core-layout__viewport'>
-      {children}
-    </div>
+    <LiveAnnouncer>
+      <NavBar {...children.props} appName={"Instructor"} />
+      <div className='core-layout__viewport'>
+        {children}
+      </div>
+    </LiveAnnouncer>
   </div>
 )
 
